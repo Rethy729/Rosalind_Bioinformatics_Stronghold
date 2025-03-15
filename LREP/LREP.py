479f676e65db046acca39cd2ab4303bbbf131b54
Rethy729 <hjn0729@gmail.com>
2025-03-16 05:07:33 +0900

    Finding the Longest Multiple Repeat

--- LREP/LREP.py
+++ LREP/LREP.py
@@ -1,12 +1,9 @@
 f = open('rosalind_lrep.txt', 'r')
 data = f.readlines()
-text = data[0].strip()[:-1]
+text = data[0].strip()
 #print(text)
 k = int(data[1].strip())
 #print(k)
-
-def sort_key(string):
-    return string.replace('$', ' ')
 
 def suffix_array(text):
     suffix_lst = []
@@ -16,8 +13,8 @@ def suffix_array(text):
         suffix_lst.append(text[i:])
         suffix_dict[text[i:]] = i
 
-    sorted_suffix_lst = sorted(suffix_lst, key = sort_key)
-
+    sorted_suffix_lst = sorted(suffix_lst)
+    print (sorted_suffix_lst)
     suffix_array = []
     for suffix in sorted_suffix_lst:
         suffix_array.append(suffix_dict[suffix])
@@ -27,6 +24,7 @@ suffix_array = suffix_array(text)
 
 def longest_common_prefix(text, suffix_array):
     common_prefix_lst = []
+    print (suffix_array)
     for i in range(1, len(text)):
         suffix_1 = text[suffix_array[i]:]
         suffix_2 = text[suffix_array[i-1]:]
@@ -38,7 +36,7 @@ def longest_common_prefix(text, suffix_array):
             h += 1
         #if common_prefix != '':
             #common_prefix_lst.append(common_prefix)
-    #print (common_prefix_lst)
+    print (common_prefix_lst)
 
     k_repeats = []
     for sub_string_1 in set(common_prefix_lst):
@@ -46,7 +44,7 @@ def longest_common_prefix(text, suffix_array):
         for sub_string_2 in common_prefix_lst:
             if sub_string_1 == sub_string_2:
                 count += 1
-        if count == k-1:
+        if count >= k-1:
             k_repeats.append(sub_string_1)
     #print (k_repeats)
     return max(k_repeats, key = len)
